472968704647b3e79d7ad019e4fd92bab27a4256
Tyler Whitehurst <twwhitehurst@gmail.com>
2020-10-18 19:46:40 -0400

    add default handler to server handlers

--- src/main/java/com/wbs/csp/messages/_MessageHandler.java
+++ src/main/java/com/wbs/csp/messages/_MessageHandler.java
@@ -10,9 +10,14 @@ import java.util.Optional;
 public abstract class _MessageHandler {
     private static final Log LOG = new Log(_MessageHandler.class);
     private final Map<Byte, Handler> handlerMap = new HashMap<>();
+    private Handler defaultHandler = null;
 
     public void bindHandler(byte type, Handler handler) {
         handlerMap.put(type, handler);
+    }
+
+    public void setDefaultHandler(Handler handler) {
+        defaultHandler = handler;
     }
 
     // todo dont know if i like this since local version will not have a packet // maybe I just ignore it though and null?
@@ -20,7 +25,13 @@ public abstract class _MessageHandler {
         try {
             LOG.trace("handling message: {}", message);
             Optional.ofNullable(handlerMap.get(message.getType()))
-                    .orElse((ignored, alsoIgnored) -> LOG.warn("Message skipped as it has no handler: {}", message))
+                    .orElse((ignored, alsoIgnored) -> {
+                        if (defaultHandler == null) {
+                            LOG.warn("Message skipped as it has no handler: {}", message);
+                        } else {
+                            defaultHandler.handle(packet, message);
+                        }
+                    })
                     .handle(packet, message);
         } catch (Exception e) {
             LOG.error("Error handling message: {}", e);
